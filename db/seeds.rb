1f998f1bd4c54c69a0b653d5b9eb62793c337c9c
sherry <sherry.kisilu@studebnt.moringaschool.com>
2023-05-01 19:54:35 +0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,9 @@ Bird.create!(name: 'Black-Capped Chickadee', species: 'Poecile Atricapillus')
 Bird.create!(name: 'Grackle', species: 'Quiscalus Quiscula')
 Bird.create!(name: 'Common Starling', species: 'Sturnus Vulgaris')
 Bird.create!(name: 'Mourning Dove', species: 'Zenaida Macroura')
-
+class AddLikesToBird < ActiveRecord::Migration[6.1]
+    def change
+      add_column :birds, :likes, :integer, null: false, default: 0
+    end
+  end
 puts '✅ Done seeding!'
